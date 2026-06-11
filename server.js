348dd737b46382be43c8571306eda09ff0508742
mogueloskar <mogueloskar@gmail.com>
2026-06-11 15:00:17 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const http = require('http')
 const handler = require('./api/index')
 
 const server = http.createServer((req, res) => {
+  res.status = (code) => { res.statusCode = code; return res }
+  res.json = (data) => { res.setHeader('Content-Type','application/json'); res.end(JSON.stringify(data)) }
+  res.send = (data) => { res.end(data) }
+  res.redirect = (code, url) => { if(typeof code === 'string'){url=code;code=302} res.statusCode=code; res.setHeader('Location',url); res.end() }
   handler(req, res)
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 server.listen(PORT, () => {
   console.log(`Oskar Briefing running on port ${PORT}`)
 })
